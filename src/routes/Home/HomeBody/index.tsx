3f4900dcb95ce6d7a8b977886090228d37656380
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-03-20 21:42:49 -0300

    Layout.

--- src/routes/Home/HomeBody/index.tsx
+++ src/routes/Home/HomeBody/index.tsx
@@ -1,7 +1,21 @@
+import Button from "../../../components/Button";
+import Card from "../../../components/Card";
+
 export default function HomeBody(){
     return (
         <main>
-            <h2>Página inicial</h2>
+            <section>
+                <div className = "container mb30 mt30">
+                    <div className = "mt30 mb30">
+                        <Card title = "Página inicial"/>
+                    </div>
+                    <div className = "dflex">
+                        <Button text = "Ver Promoção"/>
+                    </div>
+                    
+                </div>
+            </section>
+            <h2></h2>
         </main>
 
     );
